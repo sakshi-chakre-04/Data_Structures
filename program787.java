87509e832cde5c997dd9e4dd0f388996cd365495
sakshi-chakre-04 <sakshichakre0421@gmail.com>
2026-02-23 09:04:43 +0530

    Sorting & 2D Matrix

---
+++ program787.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Matix
+{
+    public int iRow;
+    public int iCol;
+    public int Arr[][];
+
+    public Matrix(int a , int b)
+    {
+        this.iRow = a;
+        this.iCol = b;
+
+        Arr = new int[iRow][iCol];
+    }
+
+}
+
+class program787
+{
+    public static void main(String args[])
+    {
+        Matrix mobj = new Matrix(4,4);
+    }
+}
