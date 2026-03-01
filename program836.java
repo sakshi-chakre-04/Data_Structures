269d09ec7322adfaeca69052858da5fe8ef1f6ae
sakshi-chakre-04 <sakshichakre0421@gmail.com>
2026-03-01 12:36:14 +0530

    Basic : Disarium  Number

---
+++ program836.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class program835
+{
+    public static void main(String[] args) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iNo = 0, iCount = 0, iTemp = 0;
+
+        System.out.println("Enter number : ");
+        iNo = sobj.nextInt();// 8
+
+        iTemp = iNo ;
+        
+        while(iNo != 0)
+        {
+            iCount++;
+            iNo = iNo / 10;
+        }
+
+        iNo = iTemp ;
+
+        while(iNo != 0)
+        {
+            iDigit = iNo % 10;
+            iSum = iSum + ((int)Math.pow(iDigit,iCount));
+            iCount--;
+            iNo = iNo / 10;
+        }
+
+        if(iSum == iTemp)
+        {
+            System.out.println(iTemp + " is Disarium Number");
+        }
+        else
+        {
+            System.out.println(iTemp + " is not Disarium Number");
+        }
+    }
+}
