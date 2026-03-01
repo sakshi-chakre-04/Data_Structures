e0404df45464137afa5a8371c63eb6cdbe276c68
sakshi-chakre-04 <sakshichakre0421@gmail.com>
2026-03-01 12:27:26 +0530

    Basic : Sunny  Number

---
+++ program834.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class program833
+{
+    public static void main(String[] args) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println((int)Math.sqrt(9));//avoid
+        System.out.println(Math.sqrt(16));
+        System.out.println(Math.sqrt(12));
+    }
+}
